53f32d1a8535b473369ee39943e8687c3dd48c8c
mooqoo <mooqooapp@gmail.com>
2017-07-22 12:34:55 -0700

    update gitignore, create a sample header module

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './header'
 
 class Index extends React.Component {
 	render() {
 		return (
-			<div>Index</div>
+			<div>
+				<Header />
+
+				<div>Whats up girl?!</div>
+			</div>
 		)
 	}
 }
 
-ReactDOM.render(<Index />, document.getElementById('App'));
+ReactDOM.render(<Index />, document.getElementById('App'))
